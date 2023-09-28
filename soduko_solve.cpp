19bd6e494a0fef43572ae407202bac19229c38cc
UthrisarKhungurBoro <121308655+Uthrisar@users.noreply.github.com>
2023-09-28 13:26:04 +0530

    Add files via upload

---
+++ soduko_solve.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        int n = board.size();
+        solveboard(board, n);
+    }
+
+    bool solveboard(vector<vector<char>> &board, int &n) {
+        bool flag = false;
+        int i, j;
+        for(i = 0; i < n; i++) {
+            for(j = 0; j < n; j++) {
+                if(board[i][j] == '.') {
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag) break;
+        }
+
+        if(i == n && j == n) return true;
+
+        for(int num = 1; num <= n; num++){
+            if(notclash(num+48, i, j, board, n)) {
+                board[i][j] = num+48;
+                if(solveboard(board, n)) return true;
+                board[i][j] = '.';
+            }
+        }
+
+        return false;
+    }
+
+       bool notclash(char num, int i, int j, vector<vector<char>> board, int n) {
+        for(int k = 0; k < n; k++) {
+            if(board[i][k] == num || board[k][j] == num) return false;
+        }
+
+        int s = sqrt(n);
+        int startrow = i - i%s;
+        int startcol = j - j%s;
+        for(int r = startrow; r < startrow+s; r++) {
+            for(int c = startcol; c < startcol+s; c++) {
+                if(board[r][c] == num) return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+
